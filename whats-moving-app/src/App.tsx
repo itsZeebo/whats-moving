75741e8495a65c4b143c09fbccfce21df9b13907
Omer <omerziv95@gmail.com>
2020-01-05 21:11:11 +0200

    added routing and design

--- whats-moving-app/src/App.tsx
+++ whats-moving-app/src/App.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
 import './App.scss';
+
+import React from 'react';
+import { HashRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+import UploadComponent from './components/UploadComponent';
+import WatchComponent from './components/WatchComponent';
+
+const ROUTES = [
+  {
+    title: "Upload",
+    url: "/upload"
+  },
+  {
+    title: "Watch",
+    url: "/watch"
+  }
+];
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <Router>
+        <div className="app-nav">
+          <img src={`${process.env.PUBLIC_URL}/assets/logo.svg`} className="logo" />
+          {ROUTES.map(route => <Link to={route.url}>{route.title}</Link>)}
+        </div>
 
+        <div className="app-content">
+          <Switch>
+            <Route exact path="/" component={UploadComponent} />
+            <Route exact path="/upload" component={UploadComponent} />
+            <Route exact path="/watch" component={WatchComponent} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ whats-moving-app/src/components/WatchComponent.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function WatchComponent() {
+    return <div>Watch page</div>;
+}
