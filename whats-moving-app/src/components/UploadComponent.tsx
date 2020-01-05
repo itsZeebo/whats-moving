a6a52becd1d49ffb1ecbea96d52dc9a17c37c49a
Omer <omerziv95@gmail.com>
2020-01-05 21:59:05 +0200

    added dropzone uploader

--- whats-moving-app/src/components/UploadComponent.tsx
+++ whats-moving-app/src/components/UploadComponent.tsx
@@ -1,5 +1,26 @@
+import "./UploadComponent.scss";
+import "react-dropzone-uploader/dist/styles.css";
+
 import React from "react";
 
+import Dropzone, { IFileWithMeta, IUploadParams, StatusValue } from "react-dropzone-uploader";
+
+const UPLOAD_URL = "http://localhost:666666666666666666666666666666666";
+
 export default function UploadComponent() {
-    return <div>Upload page</div>;
+    return <div className="upload-component">
+        <Dropzone autoUpload={false}
+            getUploadParams={_getUploadParams}
+            onChangeStatus={_handleChangeStatus}
+            onSubmit={_handleSubmit}
+            accept="video/*" />
+    </div>;
 }
+
+const _getUploadParams = (metadata: IFileWithMeta): IUploadParams => { return { url: UPLOAD_URL } }
+
+const _handleChangeStatus = (file: IFileWithMeta, status: StatusValue, allFiles: IFileWithMeta[]) =>
+    console.log(`File status changed for file: ${file.meta.name}, Status: ${status}`);
+
+const _handleSubmit = (passedFiles: IFileWithMeta[], allFiles: IFileWithMeta[]) =>
+    console.log(`Files uploaded: ${passedFiles.map(file => file.meta.name)}`);
